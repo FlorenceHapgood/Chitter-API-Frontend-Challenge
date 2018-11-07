ede33ecb91f5de2aef49d3a68f138275d185fb18
Florence Hapgood <flohapgood@icloud.com>
2018-11-07 18:17:59 +0000

    Posts have their authors displayed

--- src/app.js
+++ src/app.js
@@ -8,10 +8,13 @@ request.open('GET', 'https://chitter-backend-api.herokuapp.com/peeps', true);
 request.onload = function() {
   var dataArray = JSON.parse(this.responseText);
 
-   dataArray.map( hash => {
-     const peep = document.createElement('div');
+   dataArray.forEach( hash => {
+     const peep = document.createElement('p');
+     const author = document.createElement('p');
      peep.innerHTML = hash.body
+     author.innerHTML = "- posted by" + hash.user.handle
      document.body.appendChild(peep);
+     document.body.appendChild(author);
    })
 }
 request.send()
