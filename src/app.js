a70f9e28319e939557f261538b2031042b5e06c1
Florence Hapgood <flohapgood@icloud.com>
2018-11-07 18:01:11 +0000

    The peeps' text are appearing on the page

--- src/app.js
+++ src/app.js
@@ -6,10 +6,12 @@ request.open('GET', 'https://chitter-backend-api.herokuapp.com/peeps', true);
 
 
 request.onload = function() {
-   var dataArray = JSON.parse(this.responseText);
-   dataArray.forEach(hash =>
-     console.log(hash.body))
+  var dataArray = JSON.parse(this.responseText);
 
-   }
-
+   dataArray.map( hash => {
+     const peep = document.createElement('div');
+     peep.innerHTML = hash.body
+     document.body.appendChild(peep);
+   })
+}
 request.send()
