15c610bdbe69dbf4b03852a13623916a99500806
Florence Hapgood <flohapgood@icloud.com>
2018-11-08 17:58:12 +0000

    XML request changed for fetch, which is easier to test

--- src/app.js
+++ src/app.js
@@ -1,20 +1,38 @@
 const postElement = document.getElementById('posts');
 
-var request = new XMLHttpRequest();
+ fetch('https://chitter-backend-api.herokuapp.com/peeps')
+  .then(function(response) {
+     return response.json()
+     .then(function(response){
+      var  dataArray = response
+      dataArray.forEach( hash => {
+          const peep = document.createElement('p');
+          const author = document.createElement('p');
+          peep.innerHTML = hash.body
+          author.innerHTML = "- posted by" + hash.user.handle
+          document.body.appendChild(peep);
+          document.body.appendChild(author);
+        })
 
-request.open('GET', 'https://chitter-backend-api.herokuapp.com/peeps', true);
+     })
+  });
+  //
+  // setTimeout(function() {
+  //   console.log(response);
+  // }, 400)
 
 
-request.onload = function() {
-  var dataArray = JSON.parse(this.responseText);
 
-   dataArray.forEach( hash => {
-     const peep = document.createElement('p');
-     const author = document.createElement('p');
-     peep.innerHTML = hash.body
-     author.innerHTML = "- posted by" + hash.user.handle
-     document.body.appendChild(peep);
-     document.body.appendChild(author);
-   })
-}
-request.send()
+//
+// request.onload = function() {
+//   var dataArray = JSON.parse(this.responseText);
+//
+//   dataArray.forEach( hash => {
+//     const peep = document.createElement('p');
+//     const author = document.createElement('p');
+//     peep.innerHTML = hash.body
+//     author.innerHTML = "- posted by" + hash.user.handle
+//     document.body.appendChild(peep);
+//     document.body.appendChild(author);
+//   })
+//  }
