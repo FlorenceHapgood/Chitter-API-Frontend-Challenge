f13b248a85e9767c7c9ec0f353d158b2c2a314da
Florence Hapgood <flohapgood@icloud.com>
2019-01-26 18:35:50 +0000

    Cypress installed for feature testing

--- src/app.js
+++ src/app.js
@@ -1,21 +1,24 @@
-const postElement = document.getElementById('posts');
+//const postElement = document.getElementById('posts');
 
  fetch('https://chitter-backend-api.herokuapp.com/peeps')
-  .then(function(response) {
+  .then(function(response) { //I think the argument is automatically passed with then to the function. We could call response something else it would work.
      return response.json()
      .then(function(response){
-      var  dataArray = response
+      var dataArray = response
+      console.log(dataArray)
       dataArray.forEach( hash => {
-          const peep = document.createElement('p');
-          const author = document.createElement('p');
-          peep.innerHTML = hash.body
-          author.innerHTML = "- posted by" + hash.user.handle
-          document.body.appendChild(peep);
-          document.body.appendChild(author);
-        })
-
-     })
+        const peep = document.createElement('p');
+        const author = document.createElement('p');
+        peep.innerHTML = hash.body
+        author.innerHTML = "- posted by" + hash.user.handle
+        document.body.appendChild(peep);
+        document.body.appendChild(author);
+      })
+    })
   });
+
+
+
   //
   // setTimeout(function() {
   //   console.log(response);
