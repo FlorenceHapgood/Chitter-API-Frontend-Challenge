ba306da2468d8e57beaa3210e1ba431907da4787
Florence Hapgood <flohapgood@icloud.com>
2019-02-02 14:46:52 +0000

    Bit more styling and vacuous test deleted

--- cypress/integration/realApi.spec.js
+++ cypress/integration/realApi.spec.js
@@ -1,8 +1,4 @@
-describe ('Homepage', function() {
-  it ('Has a greeting', function() {
-    cy.visit('/');
-    cy.contains('Chitter Challenge');
-  })
+describe ('Real app', function() {
 
   it('Has an error message if the user name is taken', function(){
     cy.visit('http://localhost:8080/')
@@ -10,4 +6,5 @@ describe ('Homepage', function() {
     cy.get('#signUpSubmit').click()
     cy.contains('Handle has already been taken')
   })
+  
 });
